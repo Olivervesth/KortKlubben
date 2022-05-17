a2fff48f79999b7473543d11039b35f1630fb904
Deathpave <49186768+Deathpave@users.noreply.github.com>
2022-05-17 13:35:34 +0200

    CardManager can now generate deck(s). CardManager can now give cards, Rooms now have a constructor. Rooms now have an owner

--- CardClub/src/Cards/CardManager.java
+++ CardClub/src/Cards/CardManager.java
@@ -1,6 +1,74 @@
 package Cards;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Manager class to handle the playing cards
+ * @author Martin
+ */
 public class CardManager 
 {
-
+	/**
+	 * Fields for CardManager
+	 */
+	private List<Card> cards;	
+	
+	/**
+	 * Constructor for CardManager
+	 */
+	public CardManager()
+	{
+		cards = new ArrayList<>();
+		// can expand with multiple decks / CardManager
+		generateCardDeckNoJockers();
+	}
+	
+	/**
+	 * Method to generate a new deck of cards
+	 * This deck of cards do not contain jockers
+	 * Generated cards are added to the managers cards list
+	 */
+	public void generateCardDeckNoJockers()
+	{
+		String suit = "";
+		// for each suit
+		for(int s = 0; s < 4; s++)
+		{
+			switch(s)
+			{
+				case 0:
+					suit = "Hearts";
+					break;
+				case 1:
+					suit = "Spades";
+					break;
+				case 2:
+					suit = "Diamonds";
+					break;
+				case 3:
+					suit = "Clubs";
+					break;
+			}
+			// for each value
+			for(int v = 2; v < 14; v++)
+			{
+				cards.add(new Card(v,suit));
+			}
+		}
+	}
+	
+	/**
+	 * Method to draw a card from the cardmanager.
+	 * @return Card
+	 */
+	public Card giveCard()
+	{
+		// TODO entire method
+		int random = new Random().nextInt(0 - cards.size());
+		Card drawnCard = (Card)cards.toArray()[random];
+		cards.remove(drawnCard);
+		return drawnCard;	
+	}
 }
--- CardClub/src/Rooms/Room.java
+++ CardClub/src/Rooms/Room.java
@@ -1,5 +1,24 @@
 package Rooms;
 
-public class Room {
+import java.util.List;
 
+import Players.Player;
+
+public class Room 
+{
+	/**
+	 * Fields for Room class
+	 */
+	private List<Player> players;
+	private Player owner;
+	
+	/**
+	 * Constructor for Room class
+	 * @param Player owner
+	 */
+	public Room(Player owner)
+	{
+		this.owner = owner;
+		players.add(owner);
+	}
 }
