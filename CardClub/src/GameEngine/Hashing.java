7f5257185962c86d5fedae97c1b96e23222854a9
Deathpave <49186768+Deathpave@users.noreply.github.com>
2022-05-17 11:07:26 +0200

    Simple hashing

--- CardClub/src/GameEngine/Hashing.java
+++ CardClub/src/GameEngine/Hashing.java
@@ -1,5 +1,36 @@
 package GameEngine;
 
-public class Hashing {
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+/**
+ * Hashing class
+ * @author Martin
+ *
+ */
+public class Hashing 
+{
+	/**
+	 * Method to hash a string input
+	 * @param String input
+	 * @return String hashed input
+	 * @return null if hashing failed
+	 */
+	public String hashed(String input)
+	{
+		try 
+		{
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hashedInput = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+			return new String(hashedInput);
+		} 
+		catch (NoSuchAlgorithmException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
 }
