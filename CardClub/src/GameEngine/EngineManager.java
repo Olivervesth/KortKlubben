96ab1f6ddcf9151ca5a42849cea126cf403ad3df
Deathpave <49186768+Deathpave@users.noreply.github.com>
2022-05-19 10:11:48 +0200

    Updated EngineManager with logger class

--- CardClub/src/GameEngine/EngineManager.java
+++ CardClub/src/GameEngine/EngineManager.java
@@ -7,66 +7,61 @@ import Players.Player;
 import Players.PlayerManager;
 import Rooms.RoomManager;
 
-public final class EngineManager 
-{
+public final class EngineManager {
 	/**
 	 * Used classes in enginemanager
 	 */
 	private Hashing hashing;
-	// TODO connection string
-	// Move to ctor on EngineManager
+	private Logger logger;
 	private DbManager db;
 	private Authentication auth;
 	private static PlayerManager playerManager;
 	private static RoomManager roomManager;
-	
+
 	/**
 	 * Constructor for EngineManager
 	 */
-	public EngineManager()
-	{
+	public EngineManager() {
+		logger = new Logger();
 		hashing = new Hashing();
 		db = new DbManager("ConnectionString");
 		auth = new Authentication();
 		playerManager = new PlayerManager();
 		roomManager = new RoomManager();
 	}
-	
+
 	/**
 	 * Attempts to hash, ask database and validate the response
+	 * 
 	 * @param String username
 	 * @param String password
 	 * @return true/false
 	 */
-	public boolean login(String username, String password)
-	{
+	public boolean login(String username, String password) {
 		// if login is success
-		if(auth.validateUserInfo(db.getUserName(hashing.hash(username)), db.getPassword(hashing.hash(password))))
-		{
+		if (auth.validateUserInfo(db.getUserName(hashing.hash(username)), db.getPassword(hashing.hash(password)))) {
 			// create user?
 			return true;
 		}
 		// if login fails
-		else
-		{
+		else {
 			// log failed attempt?
 			return false;
 		}
-	}	
-	
+	}
+
 	/**
 	 * Method to reset player points
+	 * 
 	 * @param List<Player> players
 	 * @return List<Player>
 	 */
-	public static List<Player> resetPlayerPoints(List<Player> players)
-	{
+	public static List<Player> resetPlayerPoints(List<Player> players) {
 		List<Player> returnList = new ArrayList<Player>();
-		for(int i = 0; i<players.size(); i++)
-		{
+		for (int i = 0; i < players.size(); i++) {
 			returnList.add(playerManager.resetPlayerPoints(players.get(i)));
 		}
 		return returnList;
 	}
-	
+
 }
