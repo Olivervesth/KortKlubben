7ce5f8e7b2c8350af16e72a3d442d89876b6712f
Deathpave <49186768+Deathpave@users.noreply.github.com>
2022-05-19 09:30:23 +0200

    Main created to build project

--- CardClub/src/GameEngine/Logger.java
+++ CardClub/src/GameEngine/Logger.java
@@ -1,5 +1,38 @@
 package GameEngine;
 
-public class Logger {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+/**
+ * Class to log errors
+ * @author Martin
+ *
+ */
+public class Logger 
+{
+	public void saveMessage(String errormessage)
+	{
+		 try {
+			 // filepath and name
+		      File nFile = new File("filename.txt");
+		      if (nFile.createNewFile()) {
+		        System.out.println("File created: " + nFile.getName());
+		      } else {
+		    	  try {
+		    		  // filepath and name
+		    	      FileWriter fWriter = new FileWriter("filename.txt");
+		    	      fWriter.write("Files in Java might be tricky, but it is fun enough!");
+		    	      fWriter.close();
+		    	      System.out.println("Successfully wrote to the file.");
+		    	    } catch (IOException e) {
+		    	      System.out.println("An error occurred.");
+		    	      e.printStackTrace();
+		    	    }
+		      }
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+	}
 }
