d0344551cf36b3054b73c9c47c642719426a3760
Olivervesth <Olivervesth>
2022-05-24 09:53:21 +0200

    Updating Websocket endpoints

--- CardClub/src/Rooms/ClientThread.java
+++ CardClub/src/Rooms/ClientThread.java
@@ -1,5 +1,5 @@
 package Rooms;
-
+import GameEngine.EngineManager;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -8,11 +8,16 @@ import java.net.Socket;
 
 public class ClientThread extends Thread {
 	Socket client;
+	 private EngineManager em = null;
+	 private RoomManager rm = null;
 	private DataInputStream in = null;
 	public ClientThread(Socket cli) {
+		em = new EngineManager();
+		rm = new RoomManager();
 		client = cli;
 	}
 	public void run() {
+		em.saveErrorLog("Websocket","New client connected: "+client.getInetAddress()+"");
      	 System.out.println("New client connected: "+client.getInetAddress()+"");
      	 boolean clientconnected = true;
      	 System.out.println("New thread.");
@@ -35,6 +40,7 @@ public class ClientThread extends Thread {
 	    			  DataOutputStream output = new DataOutputStream(client.getOutputStream());
 	    			  output.writeUTF("200");
 	    			  line = in.readUTF();
+	    			  PlayerActions(line);
 	    			  System.out.println(line);
 	    			  
 	    		  } catch (IOException i) {
@@ -43,5 +49,60 @@ public class ClientThread extends Thread {
 	    		  }
 	    	  }
 	    	  System.out.println("Closing connection to "+client.getInetAddress()+"");
+	}
+	/**
+	 * 
+	 * @param command
+	 * @return
+	 */
+	public String PlayerActions(String command) {
+		String[] data = command.split(";");
+		
+		switch(data[0]) {
+		case "login":
+			System.out.println("in login case");
+			/**
+			 * data[1] = playername
+			 * data[2] = username
+			 * data[3] = psw
+			 */
+			try{
+				if(data[1] != null && data[2] != null && data[3] != null) {
+					
+					System.out.println("data send in login case");
+					return em.login(data[2], data[3])? "true" : "false";
+				}
+				
+			}catch(ArrayIndexOutOfBoundsException e) {
+				em.saveErrorLog(this.getName(), e.getLocalizedMessage()+""+e.getMessage());
+			}
+			
+			break;
+			
+		case "register":
+			try{
+				if(data[1] != null && data[2] != null && data[3] != null) {
+					
+					return em.createUser(data[1], data[2], data[3])? "true" : "false";
+				}
+				
+			}catch(ArrayIndexOutOfBoundsException e) {
+				em.saveErrorLog(this.getName(), e.getLocalizedMessage()+""+e.getMessage());
+			}
+					break;
+		case "createroom":
+//			try{
+//				if(data[1] != null && data[2] != null && data[3] != null) {
+//					return rm.createRoom(1,data[1])? "true" : "false";
+//				}
+//				
+//			}catch(ArrayIndexOutOfBoundsException e) {
+//				em.saveErrorLog(this.getName(), e.getLocalizedMessage()+""+e.getMessage());
+//			}
+//			break;
+
+		default:
+			return "";
+		}
 	}
 }
