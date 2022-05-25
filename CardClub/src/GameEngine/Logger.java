6a2bf58ad414a5fa473f2a8097690523f97b3082
JesperKD <Jesperd55@gmail.com>
2022-05-25 08:32:19 +0200

    Logger coms

--- CardClub/src/GameEngine/Logger.java
+++ CardClub/src/GameEngine/Logger.java
@@ -13,6 +13,14 @@ import java.io.IOException;
  */
 public class Logger {
 	
+	/**
+	 * Method to log a system message to the database
+	 * 
+	 * @param String action
+	 * @param String message
+	 * 
+	 * @returns Boolean
+	 * */
 	public boolean saveLog(String action, String message, DbManager db) {
 		try {
 
@@ -21,15 +29,26 @@ public class Logger {
 		}
 		catch(Exception ex) {
 			saveMessageLocal(action, message);
+			return true;
 		}
 	}
 	
+	
+	/**
+	 * Method to log an error message to the database
+	 * 
+	 * @param String action
+	 * @param String message
+	 * 
+	 * @returns Boolean
+	 * */
 	public boolean saveErrorLog(String action, String message, DbManager db) {
 		try {
 			System.out.println("ErrorLog Saved: " + action + " " + message);
 			return db.createErrorLog(action, message);
 		} catch(Exception ex) {
 			saveMessageLocal(action, message);
+			return true;
 		}
 	}
 	
@@ -37,7 +56,8 @@ public class Logger {
 	/**
 	 * Method to log an error message to local file
 	 * 
-	 * @param String errormessage
+	 * @param String errorAction
+	 * @param String errorMessage
 	 */
 	public void saveMessageLocal(String errorAction, String errorMessage) {
 		String homeDir = System.getProperty("user.home");
