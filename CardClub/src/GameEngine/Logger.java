2d0a0a2bdaf95b6124b9dd166beb11cf56659f91
JesperKD <Jesperd55@gmail.com>
2022-05-25 08:29:34 +0200

    Logger failsafe

--- CardClub/src/GameEngine/Logger.java
+++ CardClub/src/GameEngine/Logger.java
@@ -1,5 +1,8 @@
 package GameEngine;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 //import java.io.FileWriter;
 //import java.io.IOException;
 
@@ -11,13 +14,23 @@ package GameEngine;
 public class Logger {
 	
 	public boolean saveLog(String action, String message, DbManager db) {
-		System.out.println("Log Saved: " + action + " " + message);
-		return db.createLog(action, message);
+		try {
+
+			System.out.println("Log Saved: " + action + " " + message);
+			return db.createLog(action, message);	
+		}
+		catch(Exception ex) {
+			saveMessageLocal(action, message);
+		}
 	}
 	
 	public boolean saveErrorLog(String action, String message, DbManager db) {
-		System.out.println("ErrorLog Saved: " + action + " " + message);
-		return db.createErrorLog(action, message);
+		try {
+			System.out.println("ErrorLog Saved: " + action + " " + message);
+			return db.createErrorLog(action, message);
+		} catch(Exception ex) {
+			saveMessageLocal(action, message);
+		}
 	}
 	
 
@@ -26,17 +39,17 @@ public class Logger {
 	 * 
 	 * @param String errormessage
 	 */
-//	public void saveMessage(String errormessage, DbManager db) {
-//		String homeDir = System.getProperty("user.home");
-//		try {
-//			// filepath and name
-//			FileWriter fWriter = new FileWriter((homeDir + "/CardClubErrorLog.txt"), true);
-//			fWriter.append(errormessage + System.lineSeparator());
-//			fWriter.close();
-//			System.out.println("Successfully wrote to the file.");
-//		} catch (IOException e) {
-//			System.out.println("An error occurred.");
-//			e.printStackTrace();
-//		}
-//	}
+	public void saveMessageLocal(String errorAction, String errorMessage) {
+		String homeDir = System.getProperty("user.home");
+		try {
+			// filepath and name
+			FileWriter fWriter = new FileWriter((homeDir + "/CardClubErrorLog.txt"), true);
+			fWriter.append(errorAction + " " + errorMessage + System.lineSeparator());
+			fWriter.close();
+			System.out.println("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred during loggin locally.");
+			e.printStackTrace();
+		}
+	}
 }
